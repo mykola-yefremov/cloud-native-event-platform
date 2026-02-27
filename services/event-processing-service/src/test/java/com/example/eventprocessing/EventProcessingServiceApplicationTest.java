1553ee3382a82d426240a5db864cdb95499c071b
Mykola Yefremov <efremovnikolaus@gmail.com>
2026-02-27 20:49:32 +0100

    feat: add event processing service skeleton

---
+++ services/event-processing-service/src/test/java/com/example/eventprocessing/EventProcessingServiceApplicationTest.java
@@ -0,0 +1,10 @@
+package com.example.eventprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProcessingServiceApplicationTest {
+  @Test
+  void contextLoads() {}
+}
